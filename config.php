434c9f7747a4fc33b3f164bd9c93e884678971a0
deep-nav <navdeep.dhuti@cameronandwilding.com>
2017-01-30 13:32:44 +0000

    Removing config file

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/Facebook/autoload.php';
-session_start();
-
-$fb = new Facebook\Facebook([
-  'app_id' => '1019494864821387',
-  'app_secret' => '4f37cf1b82db804ae094bbbaf4dc928d',
-  'default_graph_version' => 'v2.5',
-]);
